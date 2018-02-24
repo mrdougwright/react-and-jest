7149298e19320e1570604e92bb2077a3bb8951e8
Doug Wright <mrdougwright@gmail.com>
2018-02-23 16:32:59 -0800

    wtf

---
+++ counter.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  state = { count: 0 }
+
+  increment = () =>
+    this.setState(prevState => ({ count: prevState.count + 1 }))
+
+  render() {
+    return (
+      <div>
+        <p>Current count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
+}
+
+export default Counter
